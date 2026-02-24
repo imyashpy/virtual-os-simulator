2252f875aecf5bff4c3b56058921d1126409970c
Yash Kirar | Backend Dev <thisisyash76@gmail.com>
2026-02-24 18:34:34 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,27 +1,9 @@
-# class Process:
-#     def __init__(self, pid):
-#         self.pid = pid
-#         self.state = "NEW"
-
-
-#day 16 adding time slice!
-# class Process:
-#     def __init__(self, pid, burst_time):
-#         self.pid = pid
-#         self.state = "NEW"
-#         self.remaining_time = burst_time
-
-
-
-#day 19
-#addding enum
 from process_state import ProcessState
-
 
 class Process:
     def __init__(self, pid, burst_time):
         self.pid = pid
-        self.state = ProcessState.NEW #new thing!
+        self.state = ProcessState.NEW 
         self.remaining_time = burst_time
 
 
