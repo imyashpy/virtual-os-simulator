cf9341df1fff0cb7165bc42bb510025ee2d4f187
Yash Kirar | Backend Dev <thisisyash76@gmail.com>
2026-02-24 18:33:05 +0530

    Update kernel.py

--- kernel.py
+++ kernel.py
@@ -1,39 +1,4 @@
-# from scheduler import Scheduler
-# from process_manager import ProcessManager
-#
-# class Kernel:
-#     def __init__(self):
-#         self.scheduler = Scheduler()
-#         self.process_manager = ProcessManager()
-#         # print("Kernel started")
-#
-#
-# #
-# # from scheduler import Scheduler
-# # from process_manager import ProcessManager
-# #
-# # class Kernel:
-# #     def __init__(self):
-# #         self.scheduler = Scheduler()
-# #         self.process_manager = ProcessManager()
-# #         print("Kernel started")
 
-
-
-#day 17
-# from scheduler import Scheduler
-# from process_manager import ProcessManager
-# from round_robin_strategy import RoundRobinStrategy
-#
-# class Kernel:
-#     def __init__(self):
-#         strategy = RoundRobinStrategy()
-#         self.scheduler = Scheduler(strategy)
-#         self.process_manager = ProcessManager()
-
-
-
-#day 18
 from fcfs_strategy import FCFSSchedulingStrategy
 from process_manager import ProcessManager
 from scheduler import Scheduler
@@ -42,6 +7,6 @@ class Kernel:
     def __init__(self):
         strategy = FCFSSchedulingStrategy()
         self.scheduler = Scheduler(strategy)
-        self.process_manager = ProcessManager()
+        self.process_manager = ProcessManager() #composition, instead of inheritence!
 
 
